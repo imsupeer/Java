e821853a48318fda8733526d6e79b1ab471577ac
Joseph Alexanndry <58670605+imsupeer@users.noreply.github.com>
2023-09-03 23:09:40 -0300

    rename and comments

--- bmi.java
+++ bmi.java
@@ -5,16 +5,16 @@ class CalculoPesoIdeal {
         Scanner scanner = new Scanner(System.in);
  
         System.out.print("Digite o seu nome: ");
-        String nome = scanner.nextLine();
+        String nome = scanner.nextLine(); //name
  
         System.out.print("Digite o seu peso (em kg): ");
-        double peso = scanner.nextDouble();
+        double peso = scanner.nextDouble(); //mass
  
         System.out.print("Digite a sua altura (em metros): ");
-        double altura = scanner.nextDouble();
+        double altura = scanner.nextDouble(); //height
  
-        double pesoIdealHomem = (72.7 * altura) - 58.0;
-        double pesoIdealMulher = (62.1 * altura) - 44.7;
+        double pesoIdealHomem = (72.7 * altura) - 58.0; //for_man
+        double pesoIdealMulher = (62.1 * altura) - 44.7; //for_woman
  
         System.out.println("Peso ideal para homem: " + pesoIdealHomem + " kg\n");
         System.out.println("Peso ideal para mulher: " + pesoIdealMulher + " kg");
